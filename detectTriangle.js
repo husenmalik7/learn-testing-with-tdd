cef47b87b6bba54d3f5acbee749fe563a81d423a
Husen <husenmalik7@gmail.com>
2025-09-14 17:45:21 +0700

    implement detectTriangle function with validation and classification logic; add comprehensive test cases

--- detectTriangle.js
+++ detectTriangle.js
@@ -1,5 +1,27 @@
-function detectTriangle(a, b, c) {
- // TODO: implement this function
+function detectTriangle(sideA, sideB, sideC) {
+	const sides = [sideA, sideB, sideC].sort((x, y) => x - y);
+	const [a, b, c] = sides;
+
+	if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
+		throw new Error("Input must be a number");
+	}
+
+	if (a <= 0) {
+		throw new Error("Input must be a positive number");
+	}
+
+	if (a + b <= c) {
+		throw new Error("Input is triangle inequality violation");
+	}
+
+	if (a === b && b === c) {
+		return "Segitiga Sama Sisi";
+	}
+
+	if (a === b || b === c) {
+		return "Segitiga Sama Kaki";
+	}
+
+	return "Segitiga Sembarang";
 }
- 
 export default detectTriangle;
--- detectTriangle.test.js
+++ detectTriangle.test.js
@@ -25,4 +25,50 @@ describe("describeTriangle function", () => {
 			"Input must be a number",
 		);
 	});
+
+	it("should throw error when input is not a positive number", () => {
+		expect(() => detectTriangle(-1, 2, 3)).toThrowError(
+			"Input must be a positive number",
+		);
+		expect(() => detectTriangle(1, -2, 3)).toThrowError(
+			"Input must be a positive number",
+		);
+		expect(() => detectTriangle(1, 2, -3)).toThrowError(
+			"Input must be a positive number",
+		);
+		// Zero is not a positive and negative number
+		expect(() => detectTriangle(0, 2, 3)).toThrowError(
+			"Input must be a positive number",
+		);
+	});
+
+	it("should throw error when input is triangle inequality violation", () => {
+		expect(() => detectTriangle(1, 2, 3)).toThrowError(
+			"Input is triangle inequality violation",
+		);
+		expect(() => detectTriangle(5, 1, 3)).toThrowError(
+			"Input is triangle inequality violation",
+		);
+		expect(() => detectTriangle(3, 4, 1)).toThrowError(
+			"Input is triangle inequality violation",
+		);
+	});
+
+	it('should return "Segitiga Sama Sisi" when all input are equal', () => {
+		expect(detectTriangle(1, 1, 1)).toBe("Segitiga Sama Sisi");
+		expect(detectTriangle(2, 2, 2)).toBe("Segitiga Sama Sisi");
+		expect(detectTriangle(3, 3, 3)).toBe("Segitiga Sama Sisi");
+	});
+
+	it('should return "Segitiga Sama Kaki" when two input are equal', () => {
+		expect(detectTriangle(2, 2, 3)).toBe("Segitiga Sama Kaki");
+		expect(detectTriangle(5, 6, 5)).toBe("Segitiga Sama Kaki");
+		expect(detectTriangle(7, 8, 8)).toBe("Segitiga Sama Kaki");
+	});
+
+	it('should return "Segitiga Sembarang" when all input are different', () => {
+		expect(detectTriangle(2, 3, 4)).toBe("Segitiga Sembarang");
+		expect(detectTriangle(5, 6, 7)).toBe("Segitiga Sembarang");
+		expect(detectTriangle(8, 9, 10)).toBe("Segitiga Sembarang");
+	});
 });
